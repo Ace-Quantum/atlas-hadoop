e10c938a6b21381995229d5894bfe63dcefded6f
Ace <acequantumbit@gmail.com>
2025-03-17 12:45:19 -0500

    task 1

---
+++ hadoop_project/lao.sh
@@ -0,0 +1,10 @@
+# This one uploads a file to the input directory on the HDFS.
+# I'm assuming that it's from wherever we decide to save it in our repo
+# There's not much direction there
+# Also, looking at the text file it seems that there's a line of Bash at the top
+# It looks like it might just be the answer???
+
+
+# Also I'm pretty sure this is how I refer to the parent directory
+
+hdfs dfs -put ../lao.txt /holbies/input
