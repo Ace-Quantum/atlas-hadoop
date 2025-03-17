96c005e4a796c8c3e4d4c67d681e5ef25f081ba2
Ace <acequantumbit@gmail.com>
2025-03-17 13:29:05 -0500

    waiting for the other shoe to drop

---
+++ hadoop_project/4-createdir.py
@@ -0,0 +1,21 @@
+# So now we're introduced into using "snakebite"
+# Which is kinda badass? It was aparantly made by the internet archive
+# Which is cool af.
+# Status update no it's not? It was made by Spotify.
+# That's not as cool of a story
+
+from snakebite.client import Client
+
+def createdir(l):
+    """l is a list of directories
+    l is also a character in DeathNote
+    That's fairly inconsequential though"""
+
+    # unsure if we need to make the port, I'll confirm with Sean when I see him
+    client = Client('localhost', 9000)
+
+    for directory in l:
+        client.mkdir([directory], create_parent=True)
+
+    # Wow it actually is that easy
+    # Nuts
